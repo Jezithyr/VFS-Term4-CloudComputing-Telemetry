20eacef9625c71d81c2ac3d4f66a8de1c90a3cfe
Sabastian <sabastianp@hotmail.ca>
2020-04-23 04:14:24 -0700

    added: line graph loads data from cloud

--- client/src/components/DataVis/SomeLineGraph.vue
+++ client/src/components/DataVis/SomeLineGraph.vue
@@ -12,6 +12,8 @@
 <script>
 
 import CustomGraph from './CustomGraph.vue'
+import { mapState } from 'vuex'
+import moment from 'moment'
 
 
 export default {
@@ -20,25 +22,50 @@ export default {
         CustomGraph
     },
 
-    data: () => ({
-        graphData: {
-            title: {
-                text: "value over time",
-                left: 'center'
-            },
-            xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [{
-                data: [820, 932, 901, 934, 1290, 1330, 1320],
-                type: 'line'
-            }]
+
+    computed: {
+        ...mapState("telemetry", {
+            entityList: state => state.entityList
+        }),
+
+        graphData: function (){
+
+            
+            // calculates the total points per player
+            let orderedEntityList = [...this.entityList].sort((a, b) => {
+                if(a.date < b.date) return -1;
+                if(b.date < a.date) return 1;
+                return 0
+            });
+
+            // moves that data into a form echarts can read
+            let data = [];
+            let axisLabels = [];
+            for(let entity of orderedEntityList){
+                data.push(entity.value)
+                axisLabels.push(moment(entity.date).format("MM/DD/YY"));
+            }
+
+            // creates the chart obj
+            return {
+                title: {
+                    text: "value over time",
+                    left: 'center'
+                },
+                xAxis: {
+                    type: 'category',
+                    data: axisLabels
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [{
+                    data: data,
+                    type: 'line'
+                }]
+            }
         }
-    }),
+    }
 }
 
 </script>
