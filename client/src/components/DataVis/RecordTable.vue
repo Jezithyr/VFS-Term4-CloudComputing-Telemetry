f30c829ad7b1a68c1805d06a5d32ae6757ee5d57
Sabastian <sabastianp@hotmail.ca>
2020-04-22 20:47:09 -0700

    refactor: moved the record table to own vue class

---
+++ client/src/components/DataVis/RecordTable.vue
@@ -0,0 +1,82 @@
+
+
+<!--
+    Copyright (C) Sabastian Peters 2020
+-->
+
+<template>
+    <div>
+        <button @click="download">refresh list</button>
+
+        <table>
+        <thead>
+            <th>Date</th>
+            <th>Session Id</th>
+            <th>Username</th>
+            <th>Position X</th>
+            <th>Position Y</th>
+            <th>Value</th>
+        </thead>
+        <tbody>
+            <tr v-for="(entity, index) in entityList" :key="index">
+                <td>{{ entity.date }}</td>
+                <td>{{ entity.sessionId }}</td>
+                <td>{{ entity.user }}</td>
+                <td>{{ entity.x }}</td>
+                <td>{{ entity.y }}</td>
+                <td>{{ entity.value }}</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+</template>
+
+
+
+<script>
+import Datastore from "@/lib/Datastore.js";
+import moment from "moment";
+
+export default {
+
+    data: () => ({
+        entityList: []
+    }),
+
+    methods: {
+        download: async function (){ // NOTE: the function keyword needs to be here to access the entity lsit
+
+            this.entityList = [{
+                date: "loading...",
+            }]
+
+            // gets the entity list and stores it in vue variable
+            let response = await Datastore.get();
+            this.entityList = response.data.entityList;
+        }
+    },
+
+    beforeMount: function(){
+        this.download();
+    }
+}
+
+</script>
+
+
+<style scoped>
+
+table {
+
+  margin: 16px;
+
+  /* Centers table */
+  margin-left:auto; 
+  margin-right:auto;
+}
+
+td {
+  padding: 12px;
+}
+
+</style>
--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -1,80 +1,22 @@
 <template>
   <div>
     <StoreTest/>
-    <button @click="download">refresh list</button>
-    <table>
-      <thead>
-        <th>Date</th>
-        <th>Session Id</th>
-        <th>Username</th>
-        <th>Position X</th>
-        <th>Position Y</th>
-        <th>Value</th>
-      </thead>
-      <tbody>
-        <tr v-for="(entity, index) in entityList" :key="index">
-          <td>{{ entity.date }}</td>
-          <td>{{ entity.sessionId }}</td>
-          <td>{{ entity.user }}</td>
-          <td>{{ entity.x }}</td>
-          <td>{{ entity.y }}</td>
-          <td>{{ entity.value }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <RecordTable/>
   </div>
 </template>
 
 
 
 <script>
-import Datastore from "@/lib/Datastore.js";
-import moment from "moment";
 import StoreTest from "@/components/StoreTest.vue"
+import RecordTable from "@/components/DataVis/RecordTable.vue"
 
 export default {
 
   components: {
-    StoreTest
+    StoreTest,
+    RecordTable
   },
-
-  data: () => ({
-    entityList: []
-  }),
-
-  methods: {
-    download: async function (){ // NOTE: the function keyword needs to be here to access the entity lsit
-
-      this.entityList = [{
-        date: "loading...",
-      }]
-
-      // gets the entity list and stores it in vue variable
-      let response = await Datastore.get();
-      this.entityList = response.data.entityList;
-    }
-  },
-
-  beforeMount: function(){
-    this.download();
-  }
+  
 };
 </script>
-
-
-<style scoped>
-
-table {
-
-  margin: 16px;
-
-  /* Centers table */
-  margin-left:auto; 
-  margin-right:auto;
-}
-
-td {
-  padding: 12px;
-}
-
-</style>
